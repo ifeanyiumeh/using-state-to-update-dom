d638a626383e4faa0b2afa25af2734773fb69809
Ifeanyi Umeh <somto_umeh@yahoo.com>
2022-03-02 00:08:35 -0500

    Finished the challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("Somto");
+  const [age, setAge] = useState(23);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2 className="subtitle is-4">Update Data from an input</h2>
+      </div>
+
+      {/* Display Data */}
+      <div className="input-display">
+        <p>Display Name: {name} </p>
+        <p>Display Age: {age}</p>
+      </div>
+
+      {/* Collect User Inputs */}
+      <div className="inputs">
+        {/* Input name */}
+        <div className="field">
+          <label className="label">Name: </label>
+          <input
+            className="input"
+            type="text"
+            placeholder="William"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        {/* Input age */}
+        <div className="field">
+          <label className="label">Age: </label>
+          <input
+            className="input"
+            type="number"
+            placeholder="38"
+            value={age}
+            onChange={(e) => setAge(+e.target.value)}
+          />
+        </div>
+      </div>
     </div>
   );
 }
